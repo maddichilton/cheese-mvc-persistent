9671d999f7ae30afd9f80da9bea9562d8e3d42fe
Maddi Chilton <maddichilton@gmail.com>
2019-06-20 20:05:13 -0500

    studio part 1 until "view all categories"

---
+++ src/main/java/org/launchcode/controllers/CategoryController.java
@@ -0,0 +1,29 @@
+package org.launchcode.controllers;
+
+import org.launchcode.models.Category;
+import org.launchcode.models.data.CategoryDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+@RequestMapping("category")
+public class CategoryController {
+
+    @Autowired
+    private CategoryDao categoryDao;
+
+    @RequestMapping(value = "")
+    public String index(Model model) {
+
+        Iterable<Category> categoryList = categoryDao.findAll();
+
+        model.addAttribute("categoryList", categoryList);
+        model.addAttribute("title", "Categories");
+
+        return "category/index";
+    }
+
+}
